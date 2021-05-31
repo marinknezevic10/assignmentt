72db8b9361efaf72092454a920dab9fe263734f6
Marin <marin.knezevic1et2@gmail.com>
2021-05-31 16:28:19 +0200

    app class

---
+++ app/core/app.php
@@ -0,0 +1,18 @@
+<?php
+
+//all the functionality starts from here
+class App
+{
+
+    //construct runs immediately
+    public function __construct()
+    {
+        print_r($_GET);
+    }
+
+    private function splitURL()
+    {
+
+    }
+}
+
