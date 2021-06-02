349bc30b1ade06019ea412c699bddfe23438b29a
Marin <marin.knezevic1et2@gmail.com>
2021-06-02 15:38:10 +0200

    instantiating model

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -5,6 +5,9 @@ class Home extends Controller //inheritance
 {
     function index()
         {
+            //instantiating model
+            $image_class = $this->loadModel("image_class");
+            //show($image_class);//checking if its an object that has returned values
             $this->view("home");//caling function from line 11, in the brackets goes name of view file
         }
     
