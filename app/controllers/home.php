29c45c55dbadc5237d8e2b6b00faece54de28d6d
Marin <marin.knezevic1et2@gmail.com>
2021-06-02 15:06:07 +0200

    home controller and 404 page

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -5,7 +5,17 @@ class Home
 {
     function index()
         {
-            echo "home page index method";
+            $this->view("home");//caling function from line 11, in the brackets goes name of view file
         }
     
+    function view($view)
+    {
+        //if the file exists show it to user
+        if(file_exists("../app/views/". $view . ".php"))
+        {
+            include "../app/views/". $view . ".php";
+        }else{//if the file doesnt exists take the user to 404 page
+            include "../app/views/404.php";
+        }
+    }
 }
