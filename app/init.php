c59df79547e7ea2fece9ea241afbd12210866d65
Marin <marin.knezevic1et2@gmail.com>
2021-06-04 15:21:42 +0200

    adding core files

--- app/init.php
+++ app/init.php
@@ -1,6 +1,8 @@
 <?php 
 //reminder: everytime we add a file in core we must include it there
 
+require "../app/core/config.php";
 require "../app/core/controller.php";
 require "../app/core/app.php";
 require "../app/core/functions.php";
+require "../app/core/database.php";
