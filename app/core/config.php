2a05c4fa97bf60f5cb7f54350338f1dc4b5a5b29
Marin <marin.knezevic1et2@gmail.com>
2021-06-04 15:28:45 +0200

    config file

--- app/core/config.php
+++ app/core/config.php
@@ -1,2 +1,37 @@
 <?php
 
+//creating constant, can be used anywhere
+//webiste name
+define("WEBSITE_NAME", "My Webiste");
+
+
+/*set database variables*/
+define('DB_TYPE','mysql');
+define('DB_NAME','mvcproject_db');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_HOST','localhost');
+
+/*protocal type http or https*/
+define('PROTOCAL','http');
+
+/*root and asset paths*/
+//checking what server it is
+$path = str_replace("\\", "/",PROTOCAL ."://" . $_SERVER['SERVER_NAME'] . __DIR__  . "/");
+$path = str_replace($_SERVER['DOCUMENT_ROOT'], "", $path);
+
+define('ROOT', str_replace("app/core", "public", $path));
+define('ASSETS', str_replace("app/core", "public/assets", $path));
+
+/*set to true to allow error reporting
+set to false when you upload online to stop error reporting*/
+
+//set to true showing errors
+define('DEBUG',true);
+
+if(DEBUG)
+{
+	ini_set("display_errors",1);
+}else{
+	ini_set("display_errors",0);
+}
