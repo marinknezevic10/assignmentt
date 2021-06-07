c0b9793a71705f60836db8ffe92c5ae81bc34718
Marin <marin.knezevic1et2@gmail.com>
2021-06-07 15:29:38 +0200

    header & footer

--- app/controllers/about.php
+++ app/controllers/about.php
@@ -5,7 +5,7 @@ class About extends Controller
     function index()
         {
             $data['page_title'] = 'About';
-            $this->view("template/about-us", $data);//caling function from line 11, in the brackets goes name of view file
+            $this->view("template/about-us", $data);
         }
    
 }
