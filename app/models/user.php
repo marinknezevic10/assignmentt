1492b3858d67c586747c6ab27df804b01a818047
Marin <marin.knezevic1et2@gmail.com>
2021-06-10 15:10:44 +0200

    user class login function

--- app/models/user.php
+++ app/models/user.php
@@ -6,6 +6,7 @@ class User
     {
         $db = new Database();
 
+        $_SESSION['error'] = "";
         if(isset($POST['username']) && isset($POST['password']))
         {
             $arr['username'] = $POST['username'];
@@ -13,7 +14,18 @@ class User
 
             $query = "select * from users where username = :username && password =:password limit 1";
             $data = $db->read($query, $arr);
-            
+            if(is_array($data))
+            {
+                //logged in
+                $_SESSION['user_id'] = $data[0]->userid;
+                $_SESSION['username'] = $data[0]->username;
+            }else{
+                
+                $_SESSION['error'] = "Wrong username or password";
+
+        }else{
+
+            $_SESSION['error'] = "Please enter valid username and password";
         }
     }
 
