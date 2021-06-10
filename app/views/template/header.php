f027df5c791493025488443455a71195f3e4eb2d
Marin <marin.knezevic1et2@gmail.com>
2021-06-10 14:49:52 +0200

    login & signup page

--- app/views/template/header.php
+++ app/views/template/header.php
@@ -25,14 +25,6 @@
       <header role="banner" class="position-absolute margin-top-30 margin-m-top-0 margin-s-top-0">    
         <!-- Top Navigation -->
         <nav class="background-transparent background-transparent-hightlight full-width sticky">
-          <div class="s-12 l-2">
-            <a href="index.html" class="logo">
-              <!-- Logo version before sticky nav -->
-              <img class="logo-before" src="<?= ASSETS ?>template/img/logo-dark.png" alt="">
-              <!-- Logo version after sticky nav -->
-              <img class="logo-after" src="<?= ASSETS ?>template/img/logo-dark.png" alt="">
-            </a>
-          </div>
           <div class="s-12 l-10">
             <div class="top-nav right">
               <p class="nav-text"></p>
