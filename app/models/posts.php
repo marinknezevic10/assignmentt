e546d8686426ff53aaa3556467b4ebd438012d6f
Marin <marin.knezevic1et2@gmail.com>
2021-06-18 14:56:56 +0200

    get all from images function

---
+++ app/models/posts.php
@@ -0,0 +1,24 @@
+<?php
+
+class Posts
+{
+    function get_all()
+    {
+        //inquiry for gathering images from db
+        $query = "select * all from images order by id desc limit 12";
+
+        //connecting to db
+        $db = new Database();
+
+        //reading the inquiry
+        $data = $db->read($query);
+        
+        //if its an array return it if its not return false
+        if(is_array($data))
+        {
+            return $data;
+        }
+            return false;
+    }
+
+}
