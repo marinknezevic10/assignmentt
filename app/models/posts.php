fde878cd19f810763bcd04f01042269ba638f075
Marin <marin.knezevic1et2@gmail.com>
2021-06-19 13:01:59 +0200

    get one post function

--- app/models/posts.php
+++ app/models/posts.php
@@ -21,4 +21,23 @@ class Posts
             return false;
     }
 
+    function get_one($link)
+    {
+        //inquiry for gathering images from db
+        $query = "select * from images where url_address = :link limit 1";
+        $arr['link'] = $link;
+        //connecting to db
+        $db = new Database();
+
+        //reading the inquiry
+        $data = $db->read($query, $arr);
+
+        //if its an array return it if its not return false
+        if(is_array($data))
+        {
+            return $data;
+        }
+            return false;
+    }
+
 }
